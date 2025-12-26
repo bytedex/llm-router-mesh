a9d49952beaefe7e0d3acfd072fe6a7d2fe76e05
bytedex <arnabduttasocial@gmail.com>
2025-12-26 16:51:30 +0530

    feat: implement basic redis get/set

--- internal/cache/redis.go
+++ internal/cache/redis.go
@@ -6,6 +6,14 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
+func NewClient(url string) (*redis.Client, error) {
+	opts, err := redis.ParseURL(url)
+	if err != nil {
+		return nil, err
+	}
+	return redis.NewClient(opts), nil
+}
+
 type RedisCache struct {
 	client *redis.Client
 	ttl    time.Duration
