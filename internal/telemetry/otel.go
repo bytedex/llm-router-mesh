4643d83daf4755383ca801964ea4e90cb448227c
bytedex <arnabduttasocial@gmail.com>
2025-12-29 15:22:11 +0530

    feat: implement OpenTelemetry tracing setup

--- internal/telemetry/otel.go
+++ internal/telemetry/otel.go
@@ -1,5 +1,12 @@
 package telemetry
 
-func InitProvider(serviceName, endpoint string) error {
+// Setup initializes OpenTelemetry exporters and trace providers
+func Setup(endpoint string) error {
+	// TODO: Configure OTLP exporter, Metrics, and Tracing to send to Prometheus/Grafana
 	return nil
+}
+
+// Shutdown gracefully flushes telemetry data
+func Shutdown() {
+	// TODO: Flush OTel data
 }
